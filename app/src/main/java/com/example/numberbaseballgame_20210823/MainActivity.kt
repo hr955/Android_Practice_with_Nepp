9e6e568db84d44d32eddf863c08f98f3fbcb6370
hr955 <nfzoze01@gmail.com>
2021-08-23 15:38:37 +0900

    문제를 471로 고정, 환영메세지 출력

--- app/src/main/java/com/example/numberbaseballgame_20210823/MainActivity.kt
+++ app/src/main/java/com/example/numberbaseballgame_20210823/MainActivity.kt
@@ -9,14 +9,16 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     val mMessageList = ArrayList<MessageData>()
-    lateinit var mAdapter: MessageAdapter
+    private lateinit var mAdapter: MessageAdapter
+
+    private val mQuestionNumbers = ArrayList<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mMessageList.add(MessageData("안녕하세요", "CPU"))
-        mMessageList.add(MessageData("반갑습니다", "USER"))
+        //난수 생성 함수
+        makeQuestionNumbers()
 
         mAdapter = MessageAdapter(this, R.layout.message_list_item, mMessageList)
         messageListView.adapter = mAdapter
@@ -33,5 +35,17 @@ class MainActivity : AppCompatActivity() {
             //리스트뷰 최하단으로 이동
             messageListView.smoothScrollToPosition(mMessageList.size - 1)
         }
+    }
+
+    private fun makeQuestionNumbers() {
+        mQuestionNumbers.add(4)
+        mQuestionNumbers.add(7)
+        mQuestionNumbers.add(1)
+
+        //환영메세지 채팅창에 띄우기
+        mMessageList.add(MessageData("어서오세요", "CPU"))
+        mMessageList.add(MessageData("숫자야구 게임입니다.", "CPU"))
+        mMessageList.add(MessageData("세자리 숫자를 맞춰주세요", "CPU"))
+
     }
 }
