aa94d1895a90d9bcdb35f65225ec93e0f0b42bcf
hr955 <nfzoze01@gmail.com>
2021-08-23 15:01:12 +0900

    어댑터/리스트뷰 연결작업

--- app/src/main/java/com/example/numberbaseballgame_20210823/MainActivity.kt
+++ app/src/main/java/com/example/numberbaseballgame_20210823/MainActivity.kt
@@ -2,18 +2,24 @@ package com.example.numberbaseballgame_20210823
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.numberbaseballgame_20210823.adapters.MessageAdapter
 import com.example.numberbaseballgame_20210823.datas.MessageData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val mMessageList = ArrayList<MessageData>()
+    lateinit var mAdapter: MessageAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mMessageList.add(MessageData("안녕하세요","CPU"))
-        mMessageList.add(MessageData("반갑습니다","USER"))
+        mMessageList.add(MessageData("안녕하세요", "CPU"))
+        mMessageList.add(MessageData("반갑습니다", "USER"))
+
+        mAdapter = MessageAdapter(this, R.layout.message_list_item, mMessageList)
+        messageListView.adapter = mAdapter
 
     }
 }
