e7d213983fd10beee74f43f627ae23f9331a038f
hr955 <nfzoze01@gmail.com>
2021-08-23 14:58:33 +0900

    연습용 메세지 데이터 추가

--- app/src/main/java/com/example/numberbaseballgame_20210823/MainActivity.kt
+++ app/src/main/java/com/example/numberbaseballgame_20210823/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.numberbaseballgame_20210823
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.numberbaseballgame_20210823.datas.MessageData
 
 class MainActivity : AppCompatActivity() {
+
+    val mMessageList = ArrayList<MessageData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mMessageList.add(MessageData("안녕하세요","CPU"))
+        mMessageList.add(MessageData("반갑습니다","USER"))
+
     }
 }
