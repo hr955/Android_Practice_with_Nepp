a7a943dd2b292bbb4fbfe6101871093b46c9b939
hr955 <nfzoze01@gmail.com>
2021-08-23 13:21:15 +0900

    KotlinUIPractice_20210819

---
+++ KotlinUIPratice_20210819/app/src/main/java/com/example/kotlinuipractie2_20210819/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.kotlinuipractie2_20210819
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnLogin.setOnClickListener {
+            if (edtID.text.toString() == "asdf" && edtPassword.text.toString() == "qwer") {
+                Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
