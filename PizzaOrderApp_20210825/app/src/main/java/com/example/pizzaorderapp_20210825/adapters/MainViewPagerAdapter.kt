fdd2e8ccf6d72509acc2e8323aad28b410cbafd3
hr955 <nfzoze01@gmail.com>
2021-08-25 10:15:03 +0900

    TabLayout + Fragment + ViewPager 생성 및 연결 작업

---
+++ PizzaOrderApp_20210825/app/src/main/java/com/example/pizzaorderapp_20210825/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,21 @@
+package com.example.pizzaorderapp_20210825.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.pizzaorderapp_20210825.fragments.OrderFragment
+import com.example.pizzaorderapp_20210825.fragments.UserFragment
+
+class mainViewPagerAdapter(val fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int = 2
+
+    override fun getItem(position: Int): Fragment = when (position) {
+        0 -> OrderFragment()
+        else -> UserFragment()
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? = when (position) {
+        0 -> "피자주문"
+        else -> "사용자 정보"
+    }
+}
