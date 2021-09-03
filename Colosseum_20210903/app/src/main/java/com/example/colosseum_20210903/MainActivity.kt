d9d25ddcc9fecfc1ebd2d20448f2b6f9a3c69502
hr955 <nfzoze01@gmail.com>
2021-09-03 17:21:06 +0900

    화면에 jsonObj 전달

--- Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/MainActivity.kt
+++ Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.colosseum_20210903.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,14 @@ class MainActivity : BaseActivity() {
             val inputId = emailEdt.text.toString()
             val inputPw = passwordEdt.text.toString()
 
-            ServerUtil.postRequestSignIn(inputId, inputPw)
+            // 서버에 이 데이터가 존재하는지 확인요청 -> 로그인 시도
+            // 서버 로그인 시도 -> 서버에 다녀오면 어떡할건지? 대응 가이드북 변수로 첨부 (인터페이스 객체)
+            ServerUtil.postRequestSignIn(inputId, inputPw, object : ServerUtil.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+                    // 서버가 보내준 jsonObj를 가지고 처리하는 코드 작성 영역
 
+                }
+            })
         }
     }
 
--- Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/utils/ServerUtil.kt
+++ Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/utils/ServerUtil.kt
@@ -8,6 +8,13 @@ import java.io.IOException
 class ServerUtil {
 
     // 단순 기능 수행 -> 서버에 요청 -> 응답을 화면에 전달
+    // 응답을 화면에 전달 : 나에게 발생한 이벤트를 화면단에게 대신 해달라고 한다. (Interface 활용)
+    interface JsonResponseHandler {
+        fun onResponse(jsonObj: JSONObject) {
+
+        }
+    }
+
     // static 함수들로 활용 -> ServerUtil.기능() 형태의 코드 작성
 
     companion object {
@@ -16,7 +23,8 @@ class ServerUtil {
         private val HOST_URL = "http://54.180.52.26"
 
         // 로그인 기능 실행 함수
-        fun postRequestSignIn(id: String, pw: String) {
+        // 아아디/패스워드 전달 + 서버에 다녀오면 어떤 일을 할건지? 인터페이스 객체 같이 전달
+        fun postRequestSignIn(id: String, pw: String, handler: JsonResponseHandler?) {
             // 1. 어디로(url) 갈것인가? HOST_URL + Endpoint
             val urlString = "${HOST_URL}/user"
 
@@ -39,7 +47,7 @@ class ServerUtil {
 
             // 만들어진 요청 호출 -> 응답이 왔을때 분석 / UI 반영
             // 호출을 하면 -> 응답 받아서 처리 (처리할 코드를 등록)
-            client.newCall(request).enqueue(object : Callback{
+            client.newCall(request).enqueue(object : Callback {
                 override fun onResponse(call: Call, response: Response) {
                     // 어떤 내용이든 응답이 돌아온 경우 (로그인 성공, 실패 모두 응답)
                     // 응답에 포함된 데이터들 중 -> 본문(Body)을 보자.
@@ -48,7 +56,10 @@ class ServerUtil {
                     val jsonObj = JSONObject(bodyString)
                     // 본문을 그냥 String 으로 찍으면 한글이 깨짐
                     // JSONObject 형태로 변환
-                   Log.d("서버 응답 본문", jsonObj.toString())
+                    Log.d("서버 응답 본문", jsonObj.toString())
+
+                    //받아낸 jsonObj를 통째로 -> 화면의 응답처리 코드에 넣어주자
+
                 }
 
                 override fun onFailure(call: Call, e: IOException) {
