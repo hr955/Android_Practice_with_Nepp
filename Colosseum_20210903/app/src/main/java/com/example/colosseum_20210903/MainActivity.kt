b373f37606525c90c5f9526cd90e9354e58ba68e
hr955 <nfzoze01@gmail.com>
2021-09-03 15:51:03 +0900

    입력한 아이디/비밀번호를 변수로 저장 - 서버의 로그인 기능활용 예정

--- Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/MainActivity.kt
+++ Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.colosseum_20210903
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +14,14 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        signInBtn.setOnClickListener {
+            //입력한 아이디와 비번을 변수로 저장 -> 서버에서 일치여부 판단
+            val inputId = emailEdt.text.toString()
+            val inputPw = passwordEdt.text.toString()
+
+            
+
+        }
     }
 
     override fun setValues() {
