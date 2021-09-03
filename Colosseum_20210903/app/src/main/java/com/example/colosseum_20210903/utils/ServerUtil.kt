c9a42d7fe37f6ff85b9c5bacaf85df9fdfad6390
hr955 <nfzoze01@gmail.com>
2021-09-03 15:42:26 +0900

    서버 통신 전담 클래스 - ServerUtil 생성 - companion object{} 함수/변수 작성예정

---
+++ Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/utils/ServerUtil.kt
@@ -0,0 +1,12 @@
+package com.example.colosseum_20210903.utils
+
+class ServerUtil {
+
+    // 단순 기능 수행 -> 서버에 요청 -> 응답을 화면에 전달
+    // static 함수들로 활용 -> ServerUtil.기능() 형태의 코드 작성
+
+    companion object{
+        // 이 안에 만드는 변수 / 함수는 전부 static 처럼 동작
+        
+    }
+}
