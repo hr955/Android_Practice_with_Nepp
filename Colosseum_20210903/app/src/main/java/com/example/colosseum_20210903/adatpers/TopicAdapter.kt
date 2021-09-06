280318d372c6edbf83681bfabfba0a1f0b5deea2
hr955 <nfzoze01@gmail.com>
2021-09-06 15:53:07 +0900

    어댑터 getView 수정 -> 실제 토론 주제를 서버에서 받아서 표시

--- Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/adatpers/TopicAdapter.kt
+++ Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/adatpers/TopicAdapter.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.example.colosseum_20210903.R
 import com.example.colosseum_20210903.datas.TopicData
 
@@ -21,6 +24,13 @@ class TopicAdapter(val mContext: Context, resId: Int, val mList: List<TopicData>
         }
 
         row!!
+
+        val data = mList[position]
+        val topicImg = row.findViewById<ImageView>(R.id.topicImg)
+        val topicTitle = row.findViewById<TextView>(R.id.topicTitleTxt)
+
+        topicTitle.text = data.title
+        Glide.with(mContext).load(data.imageUrl).into(topicImg)
 
         return row
     }
