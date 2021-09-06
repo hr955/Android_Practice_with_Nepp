de90cbd0beea3aabcd3c66a43c185879f3459893
hr955 <nfzoze01@gmail.com>
2021-09-06 15:36:53 +0900

    서버의 토픽 목록->TopicData 변환 -for문 사용 -TopicData의 보조 생성자 추가

--- Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/MainActivity.kt
+++ Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.colosseum_20210903
 
 import android.os.Bundle
+import com.example.colosseum_20210903.datas.TopicData
 import com.example.colosseum_20210903.utils.ServerUtil
 import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
+
+    val mTopicList = ArrayList<TopicData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,7 +28,15 @@ class MainActivity : BaseActivity() {
     fun getMainDataFromServer() {
         ServerUtil.getRequestMainData(mContext, object : ServerUtil.JsonResponseHandler {
             override fun onResponse(jsonObj: JSONObject) {
+                // 응답 - jsonObj 분석(파싱) -> 토론 주제들을 서버가 내려줌
+                val dataObj = jsonObj.getJSONObject("data")
+                val topicsArr = dataObj.getJSONArray("topics")
 
+                // 서버가 내려주는 토론주제들 (JsonObject 목록) -> TopicData로 변환해서 ArrayList에 추가
+                for (i in 0 until topicsArr.length()){
+                     val topicObj = topicsArr.getJSONObject(i)
+                    val tempTopicData = TopicData()
+                }
             }
         })
     }
--- Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/datas/TopicData.kt
+++ Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/datas/TopicData.kt
@@ -5,5 +5,10 @@ class TopicData(
     var title: String,
     var imageUrl: String
 ) {
+    // 보조 생성자 추가
+    constructor() : this(0,"제목없음","")
+
+    // id 값만 받는 보조 생성자
+    constructor(id: Int) : this(id,"제목없음","")
 
 }
