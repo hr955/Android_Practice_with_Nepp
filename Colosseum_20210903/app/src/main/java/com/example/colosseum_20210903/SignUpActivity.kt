a51fef89c09dbaf7c79a5e0c9cd2923c5fca8926
hr955 <nfzoze01@gmail.com>
2021-09-06 10:40:58 +0900

    회원가입 호출까지 - 응답처리 X

--- Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/SignUpActivity.kt
+++ Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/SignUpActivity.kt
@@ -2,6 +2,9 @@ package com.example.colosseum_20210903
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.colosseum_20210903.utils.ServerUtil
+import kotlinx.android.synthetic.main.activity_sign_up.*
+import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +16,19 @@ class SignUpActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        signUpBtn.setOnClickListener {
+            // 1. 입력한 값을 받아서
+            val inputEmail = emailEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
+            val inputNickname = nicknameEdt.text.toString()
+
+            // 2. 서버에 가입 요청 -> 응답에 따라 어떤 처리?
+            ServerUtil.putRequestSingUp(inputEmail, inputPw, inputNickname, object: ServerUtil.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+                    super.onResponse(jsonObj)
+                }
+            })
+        }
     }
 
     override fun setValues() {
