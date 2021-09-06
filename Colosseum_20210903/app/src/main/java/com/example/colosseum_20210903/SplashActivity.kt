b3d1a4695ff945c9a6854099e0891ccd1ec633df
hr955 <nfzoze01@gmail.com>
2021-09-06 15:57:39 +0900

    스플래쉬 화면 추가 -> 시작 화면으로 변경 - 테마 : 노 액션바

---
+++ Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.colosseum_20210903
+
+import android.os.Bundle
+
+class SplashActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        setupEvents()
+        setValues()
+
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+}
