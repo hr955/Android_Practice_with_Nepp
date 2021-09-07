bf97cd5abc5d5d6276e8ea6c0cc309cd65c03177
hr955 <nfzoze01@gmail.com>
2021-09-07 16:45:38 +0900

    BaseActivity 커스텀 액션바 적용 - 메인화면에서 활용

--- Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/BaseActivity.kt
+++ Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/BaseActivity.kt
@@ -2,7 +2,9 @@ package com.example.colosseum_20210903
 
 import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import java.math.MathContext
 
 abstract class BaseActivity : AppCompatActivity() {
@@ -16,4 +18,17 @@ abstract class BaseActivity : AppCompatActivity() {
     abstract fun setupEvents()
     abstract fun setValues()
 
+    // 커스텀 액션바로 바꿔주는 함수
+    fun setCustomActionBar(){
+        // 기본 액션바 불러오기
+        val defaultActionBar = supportActionBar!!
+
+        // 커스텀모드로 변경 -> 우리가 만든 xml로 적용
+        defaultActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        defaultActionBar.setCustomView(R.layout.my_custom_action_bar)
+
+        // 양 옆의 여백 제거 -> 모든 영역을 커스텀뷰로
+        val myToolbar = defaultActionBar.customView.parent as Toolbar
+        myToolbar.setContentInsetsAbsolute(0,0)
+    }
 }
