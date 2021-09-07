e0545259288590eb72eeb70555627ea5d0ab84a1
hr955 <nfzoze01@gmail.com>
2021-09-07 11:19:30 +0900

    기본 데이터들 실제 목록에 표시

--- Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/adatpers/ReplyAdapter.kt
+++ Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/adatpers/ReplyAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.example.colosseum_20210903.R
 import com.example.colosseum_20210903.datas.ReplyData
 
@@ -21,6 +22,21 @@ class ReplyAdapter(val mContext: Context, resId: Int, val mList: List<ReplyData>
         }
 
         row!!
+
+        val data = mList[position]
+
+        val selectedSideTxt = row.findViewById<TextView>(R.id.selectedSideTxt)
+        val writerNicknameTxt = row.findViewById<TextView>(R.id.writerNicknameTxt)
+        val createdAtTxt = row.findViewById<TextView>(R.id.createdAtTxt)
+        val contentTxt = row.findViewById<TextView>(R.id.contentTxt)
+        val replyCountTxt = row.findViewById<TextView>(R.id.replyCountTxt)
+        val likeCountTxt = row.findViewById<TextView>(R.id.likeCountTxt)
+        val hateCountTxt = row.findViewById<TextView>(R.id.hateCountTxt)
+
+        contentTxt.text = data.content
+        replyCountTxt.text = "답글 ${data.replyCount}개"
+        likeCountTxt.text = "좋아요 ${data.likeCount}개"
+        hateCountTxt.text = "싫어요 ${data.hateCount}개"
 
         return row
     }
