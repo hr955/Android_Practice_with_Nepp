266b06aeaf4d2334969d88182945d24fc9af8ad4
hr955 <nfzoze01@gmail.com>
2021-09-07 16:51:19 +0900

    supportActionBar가 실제 존재할 때에만, 커스텀 액션바 세팅 진행

--- Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/BaseActivity.kt
+++ Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/BaseActivity.kt
@@ -13,13 +13,21 @@ abstract class BaseActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         mContext = this
+
+        // 모든 액티비티의 onCreate에서 커스텀 액션바 적용
+        // 액션바가 존재하는 화면에서만 실행
+        // 액션바가 존재할때만 -> 별개의 함수를 실행
+        supportActionBar?.let {
+            //액션바가 null이 아닐때만 실행시켜줄 코드
+            setCustomActionBar()
+        }
     }
 
     abstract fun setupEvents()
     abstract fun setValues()
 
     // 커스텀 액션바로 바꿔주는 함수
-    fun setCustomActionBar(){
+    fun setCustomActionBar() {
         // 기본 액션바 불러오기
         val defaultActionBar = supportActionBar!!
 
@@ -29,6 +37,6 @@ abstract class BaseActivity : AppCompatActivity() {
 
         // 양 옆의 여백 제거 -> 모든 영역을 커스텀뷰로
         val myToolbar = defaultActionBar.customView.parent as Toolbar
-        myToolbar.setContentInsetsAbsolute(0,0)
+        myToolbar.setContentInsetsAbsolute(0, 0)
     }
 }
