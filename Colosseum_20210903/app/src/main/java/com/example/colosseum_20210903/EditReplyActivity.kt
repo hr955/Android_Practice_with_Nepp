e812c740f9d87aa4564d7aba34c6735d1deed27a
hr955 <nfzoze01@gmail.com>
2021-09-07 15:32:58 +0900

    댓글 작성 화면 추가

---
+++ Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/EditReplyActivity.kt
@@ -0,0 +1,20 @@
+package com.example.colosseum_20210903
+
+import android.os.Bundle
+
+class EditReplyActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_reply)
+
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+    }
+}
