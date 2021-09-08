5b6cccaa4f309b80fbdd172ee430b4990b132b8b
hr955 <nfzoze01@gmail.com>
2021-09-08 15:56:59 +0900

    시차 적용

--- Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/datas/NotificationData.kt
+++ Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/datas/NotificationData.kt
@@ -11,7 +11,7 @@ class NotificationData(
 
     // 생성자와 관계없이 동작하는 멤버변수
     // val : Calendar 내부의 값만 변경, 변수 자체의 대입 X
-    val createdAt = Calendar.getInstance() // default : 현재시간
+    val createdAt = Calendar.getInstance() // default : 현재시간, 디바이스에 설정된 시간대(서울)
 
     constructor() : this(0, "")
 
@@ -29,6 +29,16 @@ class NotificationData(
             // 2. 받아낸 String 을 -> Calendar에 대입 (SimpleDateFormat)
             val serverFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
             notificationData.createdAt.time = serverFormat.parse(createdAtStr)
+
+            // 3. (참고) 서버는 우리에게 UTC(GMT+0) -> 영국 시간대를 기준으로 시간을 알려줌
+            // 본인의 시간대(TimeZone)를 알아내서 -> 시간값을 조정
+            val localTimeZone = notificationData.createdAt.timeZone
+
+            // 영국과의 시차 계산
+            val timeDiff = localTimeZone.rawOffset / 1000 / 60 / 60
+
+            // 알림 생성시간(영국) + 시차 -> 한국시간
+            notificationData.createdAt.add(Calendar.HOUR, timeDiff)
 
             return notificationData
         }
