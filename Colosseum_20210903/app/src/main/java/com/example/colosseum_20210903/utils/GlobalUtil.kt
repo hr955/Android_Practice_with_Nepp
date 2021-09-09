324d56a7d7eeb76158f8ffeb2c670692bb315155
hr955 <nfzoze01@gmail.com>
2021-09-09 11:55:05 +0900

    프로젝트에서 공유할 로그인 사용자 정보를 저장할 클래스 별도 생성 - 로그인유저 변수 추가

---
+++ Colosseum_20210903/app/src/main/java/com/example/colosseum_20210903/utils/GlobalUtil.kt
@@ -0,0 +1,12 @@
+package com.example.colosseum_20210903.utils
+
+import android.service.autofill.UserData
+
+class GlobalUtil {
+    companion object{
+        // 로그인한 사용자 정보를 담아둘 변수
+        // 앱이 처음 켜졌을때는 사용자정보가 없다
+        var loginUser : UserData? = null
+
+    }
+}
